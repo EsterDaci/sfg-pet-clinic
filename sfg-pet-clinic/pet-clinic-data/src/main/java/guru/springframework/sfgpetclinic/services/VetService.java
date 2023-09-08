3799aff4b0afb4579bd2b525d73d69664e0dc9b5
Ester Daci <edaci@dalet.com>
2023-09-08 21:44:42 +0200

    Refactor Service Interfaces.

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
@@ -5,8 +5,6 @@ import guru.springframework.sfgpetclinic.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet Pet);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet,Long>{
+
 }
