b1f076fc2decae1b4b082b75520cf636a941b755
Ester Daci <edaci@dalet.com>
2023-09-24 15:14:31 +0200

    Added Speciality Entity, associated with Vets.

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,4 +1,15 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
+    private Set<Speciality> specialitySet;
+
+    public Set<Speciality> getSpecialitySet() {
+        return specialitySet;
+    }
+
+    public void setSpecialitySet(Set<Speciality> specialitySet) {
+        this.specialitySet = specialitySet;
+    }
 }
