bc4de5203f06965f113a200ae804322459c00f4c
Ester Daci <edaci@dalet.com>
2023-09-30 23:46:18 +0200

    Create Spring Dat JPA for Owners.

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -2,6 +2,9 @@ package guru.springframework.sfgpetclinic.repositories;
 
 import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.services.CrudService;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OwnerRepository extends CrudService<Owner,Long> {
+    Owner findByLastName(String lastName);
 }
--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/PetTypeRepository.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/PetTypeRepository.java
@@ -2,6 +2,8 @@ package guru.springframework.sfgpetclinic.repositories;
 
 import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.services.CrudService;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PetTypeRepository extends CrudService<PetType,Long> {
 }
