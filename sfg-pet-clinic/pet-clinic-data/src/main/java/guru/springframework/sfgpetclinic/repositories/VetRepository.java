cb9be851069b0e1d38e95c5aac699b1ecfb4a738
Ester Daci <edaci@dalet.com>
2023-10-01 14:08:22 +0200

    Create Spring Dat JPA for Vets and PetType.

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/VetRepository.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/VetRepository.java
@@ -2,6 +2,8 @@ package guru.springframework.sfgpetclinic.repositories;
 
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.CrudService;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VetRepository extends CrudService<Vet,Long> {
 }
