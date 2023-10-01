dbca46e016559111eea2f4e2f932992d3cba8361
Ester Daci <edaci@dalet.com>
2023-10-01 14:15:32 +0200

    Create Spring Data JPA for Speciality.

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/SpecialityRepository.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/SpecialityRepository.java
@@ -2,6 +2,8 @@ package guru.springframework.sfgpetclinic.repositories;
 
 import guru.springframework.sfgpetclinic.model.Speciality;
 import guru.springframework.sfgpetclinic.services.CrudService;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SpecialityRepository extends CrudService<Speciality,Long> {
 }
---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaService.java
@@ -0,0 +1,50 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import guru.springframework.sfgpetclinic.repositories.SpecialityRepository;
+import guru.springframework.sfgpetclinic.services.SpecialitiesService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialitySDJpaService implements SpecialitiesService {
+
+    private final SpecialityRepository specialityRepository;
+
+    public SpecialitySDJpaService(SpecialityRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+
+    @Override
+    public Set<Speciality> findAll() {
+        return new HashSet<>(specialityRepository.findAll());
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        Optional<Speciality> optionalSpeciality= Optional.ofNullable(specialityRepository.findById(aLong));
+
+        return optionalSpeciality.orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialityRepository.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialityRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialityRepository.deleteById(aLong);
+    }
+}
