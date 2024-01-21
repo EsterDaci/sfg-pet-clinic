fff9158d48c1f7197cd3724cf9e73e972a6998c2
Ester Daci <edaci@dalet.com>
2024-01-21 21:36:58 +0100

    Adding Forms For Pets.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -12,6 +12,13 @@ import lombok.*;
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity{
+
+    @Builder
+    public PetType(Long id, String name) {
+        super(id);
+        this.name = name;
+    }
+
     @Column(name = "name")
     private String name;
 
