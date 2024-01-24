5d5f84e116c834979352dabe61e5a21e5e306e62
Ester Daci <edaci@dalet.com>
2024-01-24 16:31:24 +0100

    Adding Create Pets.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -15,12 +15,16 @@ import java.util.Set;
 public class Owner extends Person{
 
     @Builder
-    public Owner(Long id, String firstName, String lastName, Set<Pet> pets, String address, String city, String telephone) {
+    public Owner(Long id, String firstName, String lastName, String address, String city,
+                 String telephone, Set<Pet> pets) {
         super(id, firstName, lastName);
-        this.pets = pets;
         this.address = address;
         this.city = city;
         this.telephone = telephone;
+
+        if(pets != null) {
+            this.pets = pets;
+        }
     }
 
     @OneToMany(cascade = CascadeType.ALL,mappedBy = "owner")
@@ -31,6 +35,35 @@ public class Owner extends Person{
     private String city;
     @Column(name = "telephone")
     private String telephone;
+
+    /**
+     * Return the Pet with the given name, or null if none found for this Owner.
+     *
+     * @param name to test
+     * @return true if pet name is already in use
+     */
+    public Pet getPet(String name) {
+        return getPet(name, false);
+    }
+    /**
+     * Return the Pet with the given name, or null if none found for this Owner.
+     *
+     * @param name to test
+     * @return true if pet name is already in use
+     */
+    public Pet getPet(String name, boolean ignoreNew) {
+        name = name.toLowerCase();
+        for (Pet pet : pets) {
+            if (!ignoreNew || !pet.isNew()) {
+                String compName = pet.getName();
+                compName = compName.toLowerCase();
+                if (compName.equals(name)) {
+                    return pet;
+                }
+            }
+        }
+        return null;
+    }
 
 
 }
